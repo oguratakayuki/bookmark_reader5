58043db9f2cb4b9f38c6d0781335130ca18c79c2
oguratakayuki <otn.ogura@gmail.com>
2016-09-13 06:19:47 +0900

    breadcrumbs修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def folders_breadcrumbs_list history, current_page_folder
+    breadcrumbs = []
+    breadcrumbs << {path: '', title: current_page_folder.title}
+    tmp_folder = current_page_folder
+    while tmp_folder = tmp_folder.parent_folder do
+      breadcrumbs << {path: child_folders_history_folder_path(history, tmp_folder), title: tmp_folder.title}
+    end
+    breadcrumbs << {path: root_path, title: 'Home'}
+    breadcrumbs.reverse!
+    if block_given?
+      breadcrumbs.each {|breadcrumb| yield breadcrumb }
+    end
+  end
 end
